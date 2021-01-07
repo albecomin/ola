9f46001c2128c13f851ae3f32b1f47f901fc4125
Alberto Cominetti <alb.cominetti@gmail.com>
2021-01-07 19:38:38 +0100

    correzione
    aumentata dimensione carattere pulsanti home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,14 +5,14 @@
         <b-col align="left" cols="12" style="padding-top: 32px;">
           <h1 id="home__title">Aleksandra <br> Ewa <br> Nowak</h1>
         </b-col>
-        <b-col align="right" cols="12" style="padding-top: 48px;">
-          <div class="home__button">
+        <b-col align="right" cols="12" style="padding-top: 64px;">
+          <div class="home__button" style="margin-top: 12px;">
             <router-link to="/bio" tag="div">Bio</router-link>
           </div>
-          <div class="home__button">
+          <div class="home__button" style="margin-top: 12px;">
             <router-link to="/works" tag="div">Works</router-link>
           </div>
-          <div class="home__button">
+          <div class="home__button" style="margin-top: 12px;">
             <router-link to="/about" tag="div">About</router-link>
           </div>
         </b-col>
@@ -22,18 +22,6 @@
 </template>
 
 <style lang="scss">
-.home__button {
-  width: min-content;
-  cursor: pointer;
-  user-select: none;
-  padding: 4px 16px;
-}
-.home__button:hover > div {
-  text-shadow: 0 0 1em rgba(0, 0, 0, 0.8) !important;
-}
-.home__button:active > div {
-  text-shadow: 0 0 1em rgba(0, 0, 0, 0.9) !important;
-}
 </style>
 
 <script>
