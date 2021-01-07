ac8eddf5f84edfce22ce58f9817948688f09531b
Alberto Cominetti <alb.cominetti@gmail.com>
2021-01-07 19:25:52 +0100

    rifinitura

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,25 +1,25 @@
 <template>
-  <b-container id="navbar" 
-  :class="{'bg-secondary': scrollPosition > 0}">
+  <b-container fluid id="navbar"
+  :class="{'bg-secondary shadow': scrollPosition > 0}">
     <b-row>
       <b-col cols=12>
-        <div align="center" id="navbar__home">
-          <router-link to="/" tag="div">Aleksandra <br> Ewa <br> Nowak</router-link>  
-        </div>    
-      </b-col>      
+        <div align="center" id="navbar__home" class="cursor-pointer">
+          <router-link to="/" tag="span">Aleksandra <br> Ewa <br> Nowak</router-link>
+        </div>
+      </b-col>
     </b-row>
     <b-row id="navbar_row" class="justify-content-md-center text-primary">
-      <b-col cols="4" md="auto" align="center"
-        :class="{'current': this.$route.path === '/bio'}">
-        <router-link to="/bio" tag="div" class="navbar_link">Bio</router-link>
+      <b-col cols="4" md="auto" align="center" class="navbar__link cursor-pointer"
+        :class="{'clickable': this.$route.path !== '/bio'}">
+        <router-link to="/bio" tag="span">Bio</router-link>
       </b-col>
-      <b-col cols="4" md="auto" align="center"
-      :class="{'current': this.$route.path === '/works'}">
-        <router-link to="/works" tag="div" class="navbar_link">Works</router-link>
+      <b-col cols="4" md="auto" align="center" class="navbar__link cursor-pointer"
+      :class="{'clickable': this.$route.path !== '/works'}">
+        <router-link to="/works" tag="span">Works</router-link>
       </b-col>
-      <b-col cols="4" md="auto" align="center"
-      :class="{'current': this.$route.path === '/about'}">
-        <router-link to="/about" tag="div" class="navbar_link">About</router-link>
+      <b-col cols="4" md="auto" align="center" class="navbar__link cursor-pointer"
+      :class="{'clickable': this.$route.path !== '/about'}">
+        <router-link to="/about" tag="span">About</router-link>
       </b-col>
     </b-row>
   </b-container>
@@ -50,7 +50,7 @@ export default {
 #navbar {
   font-size: 18px;
   user-select: none;
-  transition: background 0.2s;
+  transition: background 0.2s ease-in-out, box-shadow 0.1s ease-in-out;
 }
 #navbar__home {
   font-weight: bold;
@@ -58,14 +58,30 @@ export default {
   line-height: 1.2em;
   padding: 0.6em;
   opacity: 0.6;
+  transition: all 0.2s;
 }
 #navbar__home:hover {
   opacity: 0.8;
 }
+#navbar__home:hover > span {
+  text-shadow: 0 0 1em rgba(0, 0, 0, 0.3) !important;
+}
 #navbar_row {
   padding: 8px 0;
 }
-.current {
+.navbar__link {
   font-weight: bold;
+  cursor: default;
+  opacity: 0.8;
+  transition: all 0.2s;
 }
+.navbar__link.clickable:hover > span {
+  opacity: 1.0;
+  text-shadow: 0 0em 0.4em rgba(0, 0, 0, 0.3) !important;
+}
+.clickable {
+  font-weight: normal;
+  cursor: pointer !important;
+}
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,17 +6,15 @@
           <h1 id="home__title">Aleksandra <br> Ewa <br> Nowak</h1>
         </b-col>
         <b-col align="right" cols="12" style="padding-top: 48px;">
-          <b-button-group vertical size="lg">
-            <b-button variant="light" class="home__button">
-              <router-link to="/bio" tag="div">Bio</router-link>
-            </b-button>
-             <b-button variant="light" class="home__button">
-              <router-link to="/works" tag="div">Works</router-link>
-            </b-button>
-             <b-button variant="light" class="home__button">
-              <router-link to="/about" tag="div">About</router-link>
-            </b-button>
-          </b-button-group>
+          <div class="home__button">
+            <router-link to="/bio" tag="div">Bio</router-link>
+          </div>
+          <div class="home__button">
+            <router-link to="/works" tag="div">Works</router-link>
+          </div>
+          <div class="home__button">
+            <router-link to="/about" tag="div">About</router-link>
+          </div>
         </b-col>
       </b-row>
     </b-container>
@@ -24,7 +22,18 @@
 </template>
 
 <style lang="scss">
-
+.home__button {
+  width: min-content;
+  cursor: pointer;
+  user-select: none;
+  padding: 4px 16px;
+}
+.home__button:hover > div {
+  text-shadow: 0 0 1em rgba(0, 0, 0, 0.8) !important;
+}
+.home__button:active > div {
+  text-shadow: 0 0 1em rgba(0, 0, 0, 0.9) !important;
+}
 </style>
 
 <script>
